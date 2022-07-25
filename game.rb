3f186aab6ce1ff6a94dc07998bb2f78821505236
Derine Banis <d.banis.13@gmail.com>
2022-07-25 16:36:31 -0700

    Refactor. Final message in own function

--- game.rb
+++ game.rb
@@ -2,38 +2,41 @@ require "./turn.rb"
 require "./question.rb"
 
 class Game
-  attr_accessor :player_1, :player_2, :winner, :game_over
-  def initialize(player_1, player_2)
-    @player_1 = player_1
-    @player_2 = player_2
+  attr_reader :player_one, :player_two, :winner
+  def initialize(player_one, player_two)
+    @player_one = player_one
+    @player_two = player_two
     @winner = 'Nobody'
-    @game_over = false
   end
 
   def start
-    current_player = self.player_1
-    while player_1.number_of_lives > 0 && player_2.number_of_lives > 0
+    current_player = self.player_one
+    while player_one.number_of_lives > 0 && player_two.number_of_lives > 0
       question = Question.new
       turn = Turn.new(current_player, question)
       turn.start
-      unless player_1.number_of_lives == 0 || player_2.number_of_lives == 0
+      unless player_one.number_of_lives == 0 || player_two.number_of_lives == 0
         self.show_current_score
       end
-      if(current_player == self.player_1)
-        current_player = self.player_2
+      if(current_player == self.player_one)
+        current_player = self.player_two
       else 
-        current_player = self.player_1
+        current_player = self.player_one
       end
     end
-    puts "#{current_player.name} wins with a score of #{player_1.number_of_lives}/3"
-    puts "----- GAME OVER -----"
+    self.show_final_score(current_player)
+    
   end 
 
   def show_current_score
-    puts "P1: #{self.player_1.number_of_lives}/3 vs P2: #{self.player_2.number_of_lives}/3"
+    puts "P1: #{self.player_one.number_of_lives}/3 vs P2: #{self.player_two.number_of_lives}/3"
   end
 
-  # def show_final_score
+  def show_final_score(current_player)
+    puts "#{current_player.name} wins with a score of #{current_player.number_of_lives}/3"
+    puts "----- GAME OVER -----"
+    puts "Good Bye!"
+  end
       
 
   
